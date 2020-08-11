8ea10e10fe8a646375ae0424eed464569d8079d2
Jakob Richter <code@jakob-r.de>
2020-08-11 13:48:51 +0200

    working steps 2

--- R/AcqOptimizerFromOptimizer.R
+++ R/AcqOptimizerFromOptimizer.R
@@ -3,18 +3,18 @@ AcqOptimizerFromOptimizer = R6Class("AcqOptimizerFromOptimizer",
 
   public = list(
 
-    initialize = function(optimizer) {
-      private$.optimizer = assert_optimizer(optimizer)
+    term = NULL
 
+    initialize = function(optimizer, term) {
+      private$.optimizer = assert_optimizer(optimizer)
+      self$term = assert_r6(term, "Terminator")
     },
 
-    optimize = function(acqf, n_evals) {
+    optimize = function(acqf) {
       assert_r6(acqf, "AcqFunction")
-      n_evals = assert_int(n_evals)
-
+      
       inst = OptimInstanceSingleCrit$new(objective = acqf$generate_objective,
-        search_space = acqf$search_space, terminator = trm("evals",
-        n_evals = n_evals))
+        search_space = acqf$search_space, terminator = self$term)
 
       res = private$.optimizer$optimize(inst)
       res$result_y
