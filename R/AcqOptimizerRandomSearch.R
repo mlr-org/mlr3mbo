b1a9f5df7e0c715d020e090fb8a5a821a5c5aace
Marc Becker <marcbecker@posteo.de>
2020-08-11 16:22:17 +0200

    AcqOptimizerRandomSearch Draft

---
+++ R/AcqOptimizerRandomSearch.R
@@ -0,0 +1,17 @@
+#' @export
+AcqOptimizerRandomSearch = R6Class("AcqOptimizerRandomSearch",
+  inherit = AcqOptimizer,
+
+  public = list(
+    initialize = function() {
+
+    },
+
+    optimize = function(acqf) {
+      xdt = generate_design_random(acqfun$search_space, 1000)$data
+      ydt = acqfun$eval_dt(xdt)
+      xydt = cbind(xdt, ydt)
+      setorderv(xydt, acqf$codomain$ids(), order = 1, na.last = TRUE)
+      xydt[1, acqf$search_space$ids(), with = FALSE]
+    }
+))
