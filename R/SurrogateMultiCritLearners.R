84834d84e827a0197bd7c9d9126d550a0967c900
Jakob Richter <code@jakob-r.de>
2020-08-13 09:03:24 +0200

    sms works

--- R/SurrogateMultiCritLearners.R
+++ R/SurrogateMultiCritLearners.R
@@ -12,14 +12,19 @@ SurrogateMultiCritLearners = R6Class("SurrogateMultiCritLearners",
     initialize = function(learners) {
       self$model = assert_learners(learners)
       for (model in self$model) {
-        if("se" %in% self$model$predict_types) {
+        if("se" %in% model$predict_types) {
           model$predict_type = "se"
         }  
       }
     },
 
     update = function(xydt, y_cols) {
-      tasks = lapply(y_cols, function(y_col) TaskRegr$new(id = "surrogate_task", backend = xydt, target = y_col))
+      tasks = lapply(y_cols, function(y_col) {
+        TaskRegr$new(
+          id = "surrogate_task", 
+          backend = xydt[, c(setdiff(colnames(xydt), y_cols), y_col), with = FALSE], 
+          target = y_col)
+      })
       mapply(function(model, task) model$train(task), self$model, tasks)
       names(self$model) = y_cols
       invisible(NULL)
@@ -27,8 +32,8 @@ SurrogateMultiCritLearners = R6Class("SurrogateMultiCritLearners",
 
     predict = function(xdt) {
       preds = lapply(self$model, function(model) {
-        pred = self$model$predict_newdata(newdata = xdt)
-        if(self$model$predict_type == "se") {
+        pred = model$predict_newdata(newdata = xdt)
+        if(model$predict_type == "se") {
           data.table(mean = pred$response, se = pred$se)
         } else {
           data.table(mean = pred$response)
