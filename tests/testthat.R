508cecd47c648c2f3d16910d3e9c8be1aa0a176c
Marc Becker <marcbecker@posteo.de>
2020-09-21 12:08:28 +0200

    Fix AcqFunction unit tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,6 @@
 library(testthat)
 library(mlr3mbo)
+library(mlr3learners)
 
 lg
 test_check("mlr3mbo")
---
+++ tests/testthat/test_AcqFunction.R
@@ -0,0 +1,14 @@
+context("AcqFunction")
+
+test_that("AcqFunction API works", {
+  surrogate = SurrogateSingleCritLearner$new(learner = lrn("regr.km"))
+  design = generate_design_lhs(OBJ_1D$domain, 4)$data
+  inst = MAKE_INST_1D(terminator = trm("evals", n_evals = 5))
+  inst$eval_batch(design)
+
+  acqf = AcqFunction$new(id = "acqf", param_set = ParamSet$new(),
+    surrogate = surrogate, direction = "same")
+  acqf$setup(inst$archive)
+
+  acqf$generate_objective()
+})
