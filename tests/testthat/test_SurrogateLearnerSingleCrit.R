b4e44ed65b637512c5ff47e550f1d033eda1d794
sumny <lennart.sch@web.de>
2020-12-16 13:27:30 +0100

    add tests for paramset and insample performance

--- tests/testthat/test_SurrogateLearnerSingleCrit.R
+++ tests/testthat/test_SurrogateLearnerSingleCrit.R
@@ -1,5 +1,4 @@
 test_that("SurrogateSingleCritLearner API works", {
-
   surrogate = SurrogateSingleCritLearner$new(learner = REGR_KM_DETERM)
   design = generate_design_lhs(PS_1D, 4)$data
   xydt = cbind(design, OBJ_1D$eval_dt(design))
@@ -17,7 +16,6 @@ test_that("SurrogateSingleCritLearner API works", {
     fixed = "Assertion on 'target' failed: Must have length 1")
 })
 
-
 test_that("predict_types are recognized", {
   surrogate = SurrogateSingleCritLearner$new(learner = REGR_KM_DETERM)
   design = generate_design_lhs(PS_1D, 4)$data
@@ -33,5 +31,30 @@ test_that("predict_types are recognized", {
 
   surrogate$predict(xdt)
   expect_named(surrogate$predict(xdt), c("mean"))
+})
+
+test_that("param_set", {
+  surrogate = SurrogateSingleCritLearner$new(learner = REGR_KM_DETERM)
+  expect_r6(surrogate$param_set, "ParamSet")
+  expect_setequal(surrogate$param_set$ids(), c("performance_measure", "performance_epsilon"))
+  expect_r6(surrogate$param_set$params$performance_measure, "ParamUty")
+  expect_r6(surrogate$param_set$params$performance_epsilon, "ParamDbl")
+})
+
+test_that("insample_performance", {
+  surrogate = SurrogateSingleCritLearner$new(learner = REGR_KM_DETERM)
+  design = generate_design_lhs(PS_1D, 4)$data
+  xydt = cbind(design, OBJ_1D$eval_dt(design))
+
+  surrogate$update(xydt = xydt, y_cols = "y")
+  expect_double(surrogate$insample_performance, lower =  -Inf, upper = 1, any.missing = FALSE, len = 1L)
+  expect_equal(names(surrogate$insample_performance), surrogate$param_set$values$performance_measure$id)
+
+  surrogate_constant = SurrogateSingleCritLearner$new(learner = lrn("regr.featureless"))
+  surrogate_constant$update(xydt = xydt, y_cols = "y")
+  expect_double(surrogate_constant$insample_performance, lower =  -Inf, upper = 1, any.missing = FALSE, len = 1L)
+  expect_true(surrogate_constant$insample_performance == 0)
+  expect_false(surrogate_constant$test_insample_performance)
+  expect_equal(names(surrogate$test_insample_performance), surrogate$param_set$values$performance_measure$id)
 })
 
