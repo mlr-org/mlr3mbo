3263f249a09c5846102cd07e4e393ab2d6efa1f7
sumny <lennart.sch@web.de>
2020-12-17 16:23:36 +0100

    ignore vignettes

--- tic.R
+++ tic.R
@@ -1,5 +1,5 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks(args = c("--no-vignettes", "--no-build-vignettes"))
+do_package_checks(args = c("--ignore-vignettes", "--no-build-vignettes"))
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
