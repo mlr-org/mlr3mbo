cea528dd7a258b20decb06a186a096304a06592a
sumny <lennart.sch@web.de>
2020-12-17 17:38:51 +0100

    reset all tic changes

--- tic.R
+++ tic.R
@@ -1,6 +1,5 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks(args = c("--no-manual", "--as-cran", "--ignore-vignettes", "--no-multiarch"),
-  build_args = c("--no-build-vignettes"))
+do_package_checks()
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
