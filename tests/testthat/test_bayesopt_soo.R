6a70fa641a0db24aaaad23a55c4ac1725fefbf30
sumny <lennart.sch@web.de>
2020-12-22 13:42:36 +0100

    fix test

--- tests/testthat/test_bayesopt_soo.R
+++ tests/testthat/test_bayesopt_soo.R
@@ -1,5 +1,5 @@
 test_that("default bayesop_soo", {
   instance = MAKE_INST_1D(terminator = trm("evals", n_evals = 5L))
-  bayesop_soo(instance)
+  bayesopt_soo(instance)
   expect_true(nrow(instance$archive$data) == 5L)
 })
