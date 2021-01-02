7f7fc524c5a2ed85f3b251f88e03b4da779f907f
sumny <lennart.sch@web.de>
2021-01-02 20:19:10 +0100

    typo

--- tests/testthat/test_bayesopt_soo.R
+++ tests/testthat/test_bayesopt_soo.R
@@ -1,4 +1,4 @@
-test_that("default bayesop_soo", {
+test_that("default bayesopt_soo", {
   instance = MAKE_INST_1D(terminator = trm("evals", n_evals = 5L))
   bayesopt_soo(instance)
   expect_true(nrow(instance$archive$data) == 5L)
