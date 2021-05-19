71d3972a0e1c45f689225751f16d45a4de5ceb5a
Jakob Richter <code@jakob-r.de>
2021-05-19 13:39:37 +0200

    efficient acq fun intance (#47)
    * efficient acq fun intance
    
    * 0 1 scaling parego
    
    * parego works

--- R/AcqOptimizer.R
+++ R/AcqOptimizer.R
@@ -24,12 +24,12 @@ AcqOptimizer = R6Class("AcqOptimizer",
     #' @return `data.table` with 1 row per optimum and x as colums
     optimize = function(acq_function) {
       if (acq_function$codomain$length == 1) {
-        instance = OptimInstanceSingleCrit$new(objective = acq_function, terminator = self$terminator)
+        instance = OptimInstanceSingleCrit$new(objective = acq_function, terminator = self$terminator, check_values = FALSE, keep_evals = "best")
       } else {
         if (!"multi-crit" %in% self$optimizer$properties) {
           stopf("Optimizer %s is not multi-crit compatible but %s is multi-crit.", self$self$optimizer$format(), acq_function$id)
         }
-        instance = OptimInstanceMultiCrit$new(objective = acq_function, terminator = self$terminator)
+        instance = OptimInstanceMultiCrit$new(objective = acq_function, terminator = self$terminator, check_values = FALSE, keep_evals = "best")
       }
       self$optimizer$optimize(instance)
     }
