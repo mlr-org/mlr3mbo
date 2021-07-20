16eaecf8135bce09c89d6645dfb644afa8b831fa
sumny <lennart.sch@web.de>
2021-07-20 22:14:57 +0200

    vectorized version of MES

--- R/AcqFunctionMES.R
+++ R/AcqFunctionMES.R
@@ -3,7 +3,7 @@
 #' @description
 #' Max-value Entropy Search
 #'
-#' # FIXME: DESCRIPTION and Reference, tests
+#' FIXME: DESCRIPTION and Reference, tests
 #'
 #' @family Acquisition Function
 #'
@@ -39,7 +39,7 @@ AcqFunctionMES = R6Class("AcqFunctionMES",
           mu = p$mean[i]
           se = p$se[i]
           gamma = (self$maxes - (- self$surrogate_max_to_min * mu)) / se
-          p_gamma = pnorm(gamma)
+          p_gamma = stats::pnorm(gamma)
           mean(((gamma * dnorm(gamma)) / (2 * p_gamma)) - log(p_gamma), na.rm = TRUE)
         })
         mes[is.na(mes)] = 0  # FIXME: check why NAs can occur
@@ -106,19 +106,19 @@ sample_maxes_gumbel = function(x, grid, surrogate, surrogate_max_to_min, n_maxes
 
     Z = matrix(mgrid, nrow = n_grid, ncol = n_maxes, byrow = TRUE) - (- surrogate_max_to_min * matrix(mu, nrow = n_grid, ncol = n_maxes)) / matrix(se, nrow = n_grid, ncol = n_maxes)
 
-    prob = apply(pnorm(Z), MARGIN = 1L, FUN = prod)
+    prob = apply(stats::pnorm(Z), MARGIN = 1L, FUN = prod)
 
     # inverse Gumbel sampling
-    q1 = optimize(function(x) abs(probf(x, mu = mu, se = se, surrogate_max_to_min = surrogate_max_to_min) - 0.25), interval = range(mgrid))$minimum
-    q2 = optimize(function(x) abs(probf(x, mu = mu, se = se, surrogate_max_to_min = surrogate_max_to_min) - 0.50), interval = range(mgrid))$minimum
-    q3 = optimize(function(x) abs(probf(x, mu = mu, se = se, surrogate_max_to_min = surrogate_max_to_min) - 0.75), interval = range(mgrid))$minimum
+    q1 = stats::optimize(function(x) abs(probf(x, mu = mu, se = se, surrogate_max_to_min = surrogate_max_to_min) - 0.25), interval = range(mgrid))$minimum
+    q2 = stats::optimize(function(x) abs(probf(x, mu = mu, se = se, surrogate_max_to_min = surrogate_max_to_min) - 0.50), interval = range(mgrid))$minimum
+    q3 = stats::optimize(function(x) abs(probf(x, mu = mu, se = se, surrogate_max_to_min = surrogate_max_to_min) - 0.75), interval = range(mgrid))$minimum
     beta = (q1 - q3) / (log(log(4 / 3)) - log(log(4)))
     if (beta < .Machine$double.eps) beta = sqrt(.Machine$double.eps)  # FIXME: some heuristic
     alpha = q2 + beta * log(log(2))
 
     - log(- log(runif(n_maxes, min = 0, max = 1))) * beta + alpha  # FIXME: https://github.com/zi-w/Max-value-Entropy-Search/blob/master/acFuns/mesg_choose.m l61
   } else {
-    left + sd(mu) # FIXME: https://github.com/zi-w/Max-value-Entropy-Search/blob/master/acFuns/mesg_choose.m l62
+    left + stats::sd(mu) # FIXME: https://github.com/zi-w/Max-value-Entropy-Search/blob/master/acFuns/mesg_choose.m l62
   }
 }
 
@@ -127,7 +127,7 @@ sample_maxes_gumbel = function(x, grid, surrogate, surrogate_max_to_min, n_maxes
 # FIXME: document
 # this is written in the perspective of maximization, i.e., surrogate_max_to_min = -1
 probf = function(x, mu, se, surrogate_max_to_min) {
-  prod(pnorm((x - (- surrogate_max_to_min * mu)) / se))
+  prod(stats::pnorm((x - (- surrogate_max_to_min * mu)) / se))
 }
 
 
@@ -139,7 +139,8 @@ if (FALSE) {
   library(mlr3learners)
   library(ggplot2)
 
-  # global minimum of 0.397887 at (-pi, 12.275), (pi, 2.275), (9.42478, 2.475)
+  # Branin global minimum of 0.397887 at (-pi, 12.275), (pi, 2.275), (9.42478, 2.475)
+  # Eggholder global minimum of -959.6407 at (512, 404.2319)
   obfun = ObjectiveRFun$new(
     fun = function(xs) {
       a = 1
@@ -148,44 +149,48 @@ if (FALSE) {
       r = 6
       s = 10
       t = 1 / (8 * pi)
-      y = a * ((xs[["x2"]] - b * (xs[["x1"]] ^ 2) + c * xs[["x1"]] - r) ^ 2) +
-        s * (1 - t) * cos(xs[["x1"]]) + s
+      #y = a * ((xs[["x2"]] - b * (xs[["x1"]] ^ 2) + c * xs[["x1"]] - r) ^ 2) +
+      #  s * (1 - t) * cos(xs[["x1"]]) + s
+      y = -(xs[["x2"]] + 47) * sin(sqrt(abs(xs[["x2"]] + (xs[["x1"]] / 2) + 47))) -
+        xs[["x1"]] * sin(sqrt(abs(xs[["x1"]] - (xs[["x2"]] + 47))))
       list(y = y)
     },
-    domain = ParamSet$new(list(ParamDbl$new("x1", -5, 10), ParamDbl$new("x2", 0, 15))),
+    #domain = ParamSet$new(list(ParamDbl$new("x1", -5, 10), ParamDbl$new("x2", 0, 15))),
+    domain = ParamSet$new(list(ParamDbl$new("x1", -512, 512), ParamDbl$new("x2", -512, 512))),
     codomain = ParamSet$new(list(ParamDbl$new("y", tags = "minimize"))),
-    id = "Branin"
+    id = "Eggholder"
   )
   # obfun$eval_dt(data.table(x1 = c(-pi, pi, 9.42478), x2 = c(12.275, 2.275, 2.475)))
+  # obfun$eval_dt(data.table(x1 = 512, x2 = 404.2319))
 
-  obfun_ = ObjectiveRFun$new(
-    fun = function(xs) {
-      a = 1
-      b = 5.1 / (4 * (pi ^ 2))
-      c = 5 / pi
-      r = 6
-      s = 10
-      t = 1 / (8 * pi)
-      y = a * ((xs[["x2"]] - b * (xs[["x1"]] ^ 2) + c * xs[["x1"]] - r) ^ 2) +
-        s * (1 - t) * cos(xs[["x1"]]) + s
-      list(y = - y)
-    },
-    domain = ParamSet$new(list(ParamDbl$new("x1", -5, 10), ParamDbl$new("x2", 0, 15))),
-    codomain = ParamSet$new(list(ParamDbl$new("y", tags = "maximize"))),
-    id = "Branin"
-  )
+  #obfun_ = ObjectiveRFun$new(
+  #  fun = function(xs) {
+  #    a = 1
+  #    b = 5.1 / (4 * (pi ^ 2))
+  #    c = 5 / pi
+  #    r = 6
+  #    s = 10
+  #    t = 1 / (8 * pi)
+  #    y = a * ((xs[["x2"]] - b * (xs[["x1"]] ^ 2) + c * xs[["x1"]] - r) ^ 2) +
+  #      s * (1 - t) * cos(xs[["x1"]]) + s
+  #    list(y = - y)
+  #  },
+  #  domain = ParamSet$new(list(ParamDbl$new("x1", -5, 10), ParamDbl$new("x2", 0, 15))),
+  #  codomain = ParamSet$new(list(ParamDbl$new("y", tags = "maximize"))),
+  #  id = "Branin"
+  #)
 
-  terminator = trm("evals", n_evals = 30)
+  terminator = trm("evals", n_evals = 150)
 
   instance = OptimInstanceSingleCrit$new(
     objective = obfun,
     terminator = terminator
   )
 
-  instance_ = OptimInstanceSingleCrit$new(
-    objective = obfun_,
-    terminator = terminator
-  )
+  #instance_ = OptimInstanceSingleCrit$new(
+  #  objective = obfun_,
+  #  terminator = terminator
+  #)
 
   surrogate = SurrogateSingleCritLearner$new(learner = lrn("regr.km", covtype = "gauss", optim.method = "gen", nugget.stability = 10^-8))
   acq_function_mes = AcqFunctionMES$new(surrogate = surrogate)
@@ -204,12 +209,12 @@ if (FALSE) {
     instance$eval_batch(design)
     ei_res = bayesopt_soo(instance, acq_function_ei, acq_optimizer)$clone(deep = TRUE)
 
-    instance_$archive$clear()
-    instance_$eval_batch(design)
-    mes_res_ = bayesopt_soo(instance_, acq_function_mes, acq_optimizer)$clone(deep = TRUE)
-    instance_$archive$clear()
-    instance_$eval_batch(design)
-    ei_res_ = bayesopt_soo(instance_, acq_function_ei, acq_optimizer)$clone(deep = TRUE)
+    #instance_$archive$clear()
+    #instance_$eval_batch(design)
+    #mes_res_ = bayesopt_soo(instance_, acq_function_mes, acq_optimizer)$clone(deep = TRUE)
+    #instance_$archive$clear()
+    #instance_$eval_batch(design)
+    #ei_res_ = bayesopt_soo(instance_, acq_function_ei, acq_optimizer)$clone(deep = TRUE)
 
     get_trace = function(archive, inverted = FALSE) {
       tmp = map_dtr(unique(archive$data$batch_nr), function(bn) archive$best(batch = 1:bn))
@@ -217,7 +222,7 @@ if (FALSE) {
         tmp$y = - tmp$y
       }
       tmp$iteration = seq_len(nrow(tmp))
-      tmp$regret = tmp$y - 0.397887
+      tmp$regret = tmp$y - -959.6407
       tmp
     }
     mes_resx = get_trace(mes_res)
@@ -227,14 +232,14 @@ if (FALSE) {
     ei_resx$method = "ei"
     ei_resx$i = i
 
-    mes_resx_ = get_trace(mes_res_, inverted = TRUE)
-    mes_resx_$method = "mes_"
-    mes_resx_$i = i
-    ei_resx_ = get_trace(ei_res_, inverted = TRUE)
-    ei_resx_$method = "ei__"
-    ei_resx_$i = i
+    #mes_resx_ = get_trace(mes_res_, inverted = TRUE)
+    #mes_resx_$method = "mes_"
+    #mes_resx_$i = i
+    #ei_resx_ = get_trace(ei_res_, inverted = TRUE)
+    #ei_resx_$method = "ei__"
+    #ei_resx_$i = i
 
-    res = rbind(res, mes_resx, ei_resx, mes_resx_, ei_resx_, fill = TRUE)
+    res = rbind(res, mes_resx, ei_resx, fill = TRUE)
   }
 
   dat = setNames(res[, mean(regret), by = .(iteration, method)], c("iteration", "method", "mean_regret"))
@@ -243,7 +248,7 @@ if (FALSE) {
   dat$upper = dat$mean_regret + dat$se
   dat$lower = dat$mean_regret - dat$se
 
-  ggplot(aes(x = iteration, y = log(mean_regret), colour = method, fill = method), data = dat) +
+  ggplot(aes(x = iteration, y = mean_regret, colour = method, fill = method), data = dat) +
     geom_line() +
     geom_ribbon(aes(x = iteration, max = log(upper), min = log(lower)), alpha = 0.25, colour = NA)
 }
