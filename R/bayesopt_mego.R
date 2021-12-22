f68817de71237282e735139c60b490da850d71aa
Lennart Schneider <lennart.sch@web.de>
2021-12-22 01:31:19 +0100

    mego fixes

--- R/bayesopt_mego.R
+++ R/bayesopt_mego.R
@@ -103,17 +103,15 @@ bayesopt_mego = function(
   }
 
   # generate a multicriteria acquisition function based on the singlecriteria ones
+  acq_function_domain = instance$search_space$clone(deep = TRUE)
+  acq_function_domain$trafo = NULL
+
   acq_function_codomain = ParamSet$new(map(acq_functions, function(acq_function) {
     id = paste0(acq_function$id, "_", acq_function$surrogate$y_cols)
     ParamDbl$new(id = id, tags = unlist(acq_function$codomain$tags, use.names = FALSE))
   }))
-  acq_function = ObjectiveRFunDt$new(
-    fun = function(xdt) {
-      setNames(map_dtc(acq_functions, function(acqf) acqf$eval_dt(xdt)), nm = acq_function_codomain$ids())
-    },
-    domain = instance$search_space$clone(deep = TRUE),
-    codomain = acq_function_codomain
-  )
+
+  acq_function = AcqFunctionMulti$new("multi", acq_functions = acq_functions, domain = acq_function_domain, codomain = acq_function_codomain, archive = archive)
   class(acq_function) = c(class(acq_function), "AcqFunction")  # FIXME:
 
   if (is.null(acq_optimizer)) acq_optimizer = default_acqopt(acq_function)
@@ -128,7 +126,7 @@ bayesopt_mego = function(
   # loop
   repeat {
     xdt = tryCatch({
-      # FIXME:
+      # FIXME: this should go into multi class below
       invisible(map(acq_functions, function(acq_function) {
         acq_function$surrogate$update()
         acq_function$update()
@@ -149,4 +147,98 @@ bayesopt_mego = function(
 
   return(invisible(instance))
 }
+
+
+
+AcqFunctionMulti = R6Class("AcqFunctionMulti",
+  inherit = bbotk::Objective,
+  public = list(
+
+    #' @field acq_functions (`list` of [AcqFunction]s).
+    acq_functions = NULL,
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    #'
+    #' @param id (`character(1)`).
+    #' @param acq_functions (`list` of [AcqFunction]s).
+    #' @param domain ([paradox::ParamSet]).
+    #' @param codomain ([paradox::ParamSet]).
+    #' @param archive ([bbotk::Archive]).
+    initialize = function(id, acq_functions, domain, codomain, archive) {
+      assert_string(id)
+      self$acq_functions = assert_list(acq_functions, type = "AcqFunction")
+      private$.archive = assert_r6(archive, "Archive")
+
+      super$initialize(
+        id = id,
+        domain = domain,
+        codomain = codomain,
+        # FIXME: constants?
+      )
+    },
+
+    #' @description
+    #' Evaluates multiple input values received as a list, converted to a `data.table()` on the
+    #' objective function. Missing columns in xss are filled with `NA`s in `xdt`.
+    #'
+    #' @param xss (`list()`)\cr
+    #'   A list of lists that contains multiple x values, e.g.
+    #'   `list(list(x1 = 1, x2 = 2), list(x1 = 3, x2 = 4))`.
+    #'
+    #' @return [data.table::data.table()] that contains one y-column for single-criteria functions
+    #' and multiple y-columns for multi-criteria functions, e.g.
+    #' `data.table(y = 1:2)` or `data.table(y1 = 1:2, y2 = 3:4)`.
+    eval_many = function(xss) {
+      if (self$check_values) lapply(xss, self$domain$assert)
+      res = invoke(private$.fun, rbindlist(xss, use.names = TRUE, fill = TRUE), .args = self$constants$values)
+      if (self$check_values) self$codomain$assert_dt(res[, self$codomain$ids(), with = FALSE])
+      res
+    },
+
+    #' @description
+    #' Evaluates multiple input values on the objective function supplied by the user.
+    #'
+    #' @param xdt ([data.table::data.table()])\cr
+    #'   Set of untransformed points / points from the *search space*.
+    #'   One point per row, e.g. `data.table(x1 = c(1, 3), x2 = c(2, 4))`.
+    #'   Column names have to match ids of the `search_space`.
+    #'   However, `xdt` can contain additional columns.
+    #'
+    #' @return data.table::data.table()] that contains one y-column for single-criteria functions
+    #' and multiple y-columns for multi-criteria functions, e.g.
+    #' `data.table(y = 1:2)` or `data.table(y1 = 1:2, y2 = 3:4)`.
+    eval_dt = function(xdt) {
+      if (self$check_values) self$domain$assert_dt(xdt)
+      res = invoke(private$.fun, xdt, .args = self$constants$values)
+      if (self$check_values) self$codomain$assert_dt(res[, self$codomain$ids(), with = FALSE])
+      res
+    }
+  ),
+
+  active = list(
+    #' @field archive ([bbotk::Archive])\cr
+    #'   Points to the [bbotk::Archive] of the [Surrogate]s of the [AcqFunction]s
+    archive = function(rhs) {
+      if (!missing(rhs) && !identical(rhs, private$.archive)) stop("archive is read-only.")
+      private$.archive
+    },
+
+    #' @field fun (`function`)\cr
+    #'   Objective function.
+    fun = function(lhs) {
+      if (!missing(lhs) && !identical(lhs, private$.fun)) stop("fun is read-only.")
+      private$.fun
+    }
+  ),
+
+  private = list(
+    .archive = NULL,
+
+    .fun = function(xdt) {
+      xss = transform_xdt_to_xss(xdt, self$domain)
+      setNames(map_dtc(self$acq_functions, function(acqf) acqf$eval_many(xss)), nm = self$codomain$ids())
+    }
+  )
+)
 
