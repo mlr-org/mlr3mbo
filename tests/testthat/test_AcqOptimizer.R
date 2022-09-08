9aab6213561b763a865c4152dc69eb8c02dd91f4
Lennart Schneider <lennart.sch@web.de>
2022-09-08 16:06:40 +0200

    test: AcqOptimizer

--- tests/testthat/test_AcqOptimizer.R
+++ tests/testthat/test_AcqOptimizer.R
@@ -1,66 +1,65 @@
 test_that("AcqOptimizer API works", {
-  with_seed(1, {
-    # EI, random search
-    instance = OptimInstanceSingleCrit$new(OBJ_1D, terminator = trm("evals", n_evals = 5L))
-    design = MAKE_DESIGN(instance)
-    instance$eval_batch(design)
-    acqfun = AcqFunctionEI$new(SurrogateLearner$new(REGR_KM_DETERM, archive = instance$archive))
-    acqopt = AcqOptimizer$new(opt("random_search", batch_size = 2L), trm("evals", n_evals = 2L), acq_function = acqfun)
-    acqfun$surrogate$update()
-    acqfun$update()
-    expect_data_table(acqopt$optimize(), nrows = 1L)
+  # EI, random search
+  instance = OptimInstanceSingleCrit$new(OBJ_1D, terminator = trm("evals", n_evals = 5L))
+  with_seed(29, {design = MAKE_DESIGN(instance)})
+  instance$eval_batch(design)
+  acqfun = AcqFunctionEI$new(SurrogateLearner$new(REGR_KM_DETERM, archive = instance$archive))
+  acqopt = AcqOptimizer$new(opt("random_search", batch_size = 2L), trm("evals", n_evals = 2L), acq_function = acqfun)
+  with_seed(24, {acqfun$surrogate$update()})
+  acqfun$update()
+  expect_data_table(acqopt$optimize(), nrows = 1L)
 
-    # upgrading error class works - catch_errors
-    acqopt = AcqOptimizer$new(OptimizerError$new(), trm("evals", n_evals = 2L), acq_function = acqfun)
-    expect_error(acqopt$optimize(), class = c("mbo_error", "optimize_error"))
+  # upgrading error class works - catch_errors
+  acqopt = AcqOptimizer$new(OptimizerError$new(), trm("evals", n_evals = 2L), acq_function = acqfun)
+  expect_error(acqopt$optimize(), class = c("mbo_error", "optimize_error"))
 
-    acqopt$param_set$values$catch_errors = FALSE
-    expect_error(acqopt$optimize(), class = c("simpleError", "condition"))
+  acqopt$param_set$values$catch_errors = FALSE
+  expect_error(acqopt$optimize(), class = c("simpleError", "condition"))
 
-    # logging_level
-    console_appender = if (packageVersion("lgr") >= "0.4.0") lg$inherited_appenders$console else lg$inherited_appenders$appenders.console
-    f = tempfile("bbotklog_", fileext = "log")
-    th1 = lg$threshold
-    th2 = console_appender$threshold
+  # logging_level
+  console_appender = if (packageVersion("lgr") >= "0.4.0") lg$inherited_appenders$console else lg$inherited_appenders$appenders.console
+  f = tempfile("bbotklog_", fileext = "log")
+  th1 = lg$threshold
+  th2 = console_appender$threshold
 
-    lg$set_threshold("debug")
-    lg$add_appender(lgr::AppenderFile$new(f, threshold = "debug"), name = "testappender")
-    console_appender$set_threshold("warn")
+  lg$set_threshold("debug")
+  lg$add_appender(lgr::AppenderFile$new(f, threshold = "debug"), name = "testappender")
+  console_appender$set_threshold("warn")
 
-    on.exit({
-      lg$remove_appender("testappender")
-      lg$set_threshold(th1)
-      console_appender$set_threshold(th2)
-    })
-
-    acqopt = AcqOptimizer$new(opt("random_search", batch_size = 2L), trm("evals", n_evals = 2L), acq_function = acqfun)
-    acqopt$param_set$values$logging_level = "warn"
-    acqopt$optimize()
-    lines = readLines(f)
-    expect_equal(lines, character(0))
-
-    acqopt$param_set$values$logging_level = "info"
-    acqopt$optimize()
-    lines = readLines(f)
-    expect_character(lines, min.len = 1L)
-
-    # warmstart | warmstart_size | skip_already_evaluated
-    acqopt = AcqOptimizer$new(opt("random_search", batch_size = 1L), trm("evals", n_evals = 4L), acq_function = acqfun)
-    acqopt$param_set$values$warmstart = TRUE
-    expect_true(with_seed(1, {acqopt$optimize()[["acq_ei"]]}) > sqrt(.Machine$double.eps))
-
-    acqopt$param_set$values$warmstart_size = 1L
-    expect_true(with_seed(2, {acqopt$optimize()[["acq_ei"]]}) > sqrt(.Machine$double.eps))
-
-    acqopt$param_set$values$warmstart_size = "all"
-    expect_error(acqopt$optimize(), "All candidates were already evaluated.")
-
-    acqopt$param_set$values$skip_already_evaluated = FALSE
-    expect_true(with_seed(3, {acqopt$optimize()[["acq_ei"]]}) < sqrt(.Machine$double.eps))
-
-    acqopt$param_set$values$warmstart = FALSE
-    expect_true(with_seed(4, {acqopt$optimize()[["acq_ei"]]}) > sqrt(.Machine$double.eps))
+  on.exit({
+    lg$remove_appender("testappender")
+    lg$set_threshold(th1)
+    console_appender$set_threshold(th2)
   })
+
+  acqopt = AcqOptimizer$new(opt("random_search", batch_size = 2L), trm("evals", n_evals = 2L), acq_function = acqfun)
+  acqopt$param_set$values$logging_level = "warn"
+  acqopt$optimize()
+  lines = readLines(f)
+  expect_equal(lines, character(0))
+
+  acqopt$param_set$values$logging_level = "info"
+  acqopt$optimize()
+  lines = readLines(f)
+  expect_character(lines, min.len = 1L)
+
+  # warmstart | warmstart_size | skip_already_evaluated
+  acqopt = AcqOptimizer$new(opt("random_search", batch_size = 1L), trm("evals", n_evals = 4L), acq_function = acqfun)
+  acqopt$param_set$values$warmstart = TRUE
+  expect_true(with_seed(1, {acqopt$optimize()[["acq_ei"]]}) > sqrt(.Machine$double.eps))
+
+  acqopt$param_set$values$warmstart_size = 1L
+  expect_true(with_seed(2, {acqopt$optimize()[["acq_ei"]]}) > sqrt(.Machine$double.eps))
+
+  acqopt$param_set$values$warmstart_size = "all"
+  expect_error(acqopt$optimize(), "All candidates were already evaluated.")
+
+  acqopt$param_set$values$skip_already_evaluated = FALSE
+  expect_true(with_seed(3, {acqopt$optimize()[["acq_ei"]]}) < sqrt(.Machine$double.eps))
+
+  acqopt$param_set$values$warmstart_size = NULL
+  acqopt$param_set$values$warmstart = FALSE
+  expect_true(with_seed(4, {acqopt$optimize()[["acq_ei"]]}) > sqrt(.Machine$double.eps))
 })
 
 test_that("AcqOptimizer param_set", {
@@ -98,7 +97,7 @@ test_that("AcqOptimizer trafo", {
 
 test_that("AcqOptimizer deep clone", {
   acqopt1 = AcqOptimizer$new(opt("random_search", batch_size = 1L), trm("evals", n_evals = 1L))
-  acqopt2 = acqopt$clone(deep = TRUE)
+  acqopt2 = acqopt1$clone(deep = TRUE)
   expect_true(address(acqopt1) != address(acqopt2))
   expect_true(address(acqopt1$optimizer) != address(acqopt2$optimizer))
   expect_true(address(acqopt1$terminator) != address(acqopt2$terminator))
