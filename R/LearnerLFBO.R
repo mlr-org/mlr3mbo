02af5655a62a57a676e02d6843c75c68e683d40c
pfistfl <pfistererf@googlemail.com>
2023-02-01 14:46:20 +0100

    fix impl errors, add docs

---
+++ R/LearnerLFBO.R
@@ -0,0 +1,99 @@
+#' @title Likelihood Free Bayesian Optimization Learner
+#' 
+#' @description 
+#' Wraps a classification learner to be used as a regression learner for
+#' Likelihood-free Optimization. 
+#' 
+#' @details 
+#' The regression data is internally converted to a weighted classification task.
+#' The new objective is a weighted version of the prediction problem, where the goal
+#' is to predict whether an instance is smaller than the `gamma` quantile of the target 
+#' distribution.
+#' 
+#' @export
+LearnerLFBO = R6Class("LearnerLFBO",
+  inherit = mlr3::LearnerRegr,
+  public = list(
+
+    #' @field clf ([mlr3::LearnerClassif])\cr
+    #'   Classifier to be used for LFBO.
+    clf = NULL,
+    #' @field wt (character)\cr
+    #'   Weighting scheme for LFBO. Currently allows "ei" and "pi".
+    wt = NULL,
+    #' @field gamma (numeric)\cr
+    #'   Quantile of the target distribution. Defaults to `0.33`. 
+    gamma = NULL,
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    #'
+    #' @param clf ([mlr3::LearnerClassif])\cr
+    #'   Classifier to be used for LFBO. Needs to have predict_type "prob".
+    #' @param wt (character)\cr
+    #'   Weighting scheme for LFBO. Currently allows "ei" (expected improvement) and "pi" (probability of improvement).
+    #' @param gamma (numeric)\cr
+    #'   Quantile of the target distribution. Defaults to `0.33`. 
+    initialize = function(clf, gamma = 0.33, wt = "ei") {
+      assert_class(clf, "LearnerClassif")
+      assert_true(clf$predict_type == "prob")
+      assert_true("weights" %in% clf$properties)
+      self$clf = clf
+      self$gamma = assert_number(gamma, lower = 0, upper = 1)
+      self$wt = assert_choice(wt, choices = c("ei", "pi"))
+      props = setdiff(clf$properties, c("twoclass", "multiclass"))
+      super$initialize(
+        id = paste0(clf$id, "2regr"),
+        param_set = clf$param_set, feature_types = clf$feature_types,
+        predict_types = c("response"),
+        properties = props, data_formats = clf$data_formats, packages = clf$packages,
+        label = clf$label, man = clf$man
+      )
+    },
+    train = function(task, row_ids = NULL) {
+        # create df with new data.
+        tn = private$.to_weighted_task(task, row_ids)
+        self$clf$train(tn, row_ids)
+        self$state$train_task = mlr3:::task_rm_backend(task$clone(deep = TRUE))
+        invisible(NULL)
+    },
+    predict = function(task, row_ids = NULL) {
+      fts = task$data(rows = row_ids, cols = task$feature_names)
+      colnames(fts) = paste0("X.", colnames(fts))
+      pd = self$clf$predict_newdata(fts)$data
+      pdata = as_prediction_data(
+        list(response = - pd$prob[, 2L]),
+        task = task, 
+        row_ids = pd$row_ids
+      )
+      as_prediction(pdata)
+    }
+  ),
+  private = list(
+    .to_weighted_task = function(task, row_ids) {
+        dt = do.call("cbind", private$.regr_to_weights(
+            task$data(rows = row_ids, cols = task$feature_names),
+            task$data(rows = row_ids, cols = task$target_names)[[1]]
+        ))
+        tn = TaskClassif$new("weighted_classif", dt, target = "z")
+        tn$set_col_roles("w", "weight")
+    },
+    .regr_to_weights = function(X,y, gamma = .8, wt = "ei") {
+        tau = quantile(y, probs = self$gamma)
+        z = y < tau
+        if (self$wt == "ei") {
+            w1 = tau - y[z]
+            w1 = w1 / mean(w1)
+            Xn = rbind(X, X[z,])
+            zn = c(rep(0, length(z)), z[z])
+            wn = c(rep(1, length(z)) / length(z), w1 * sum(z))
+            wn = wn / mean(wn)
+        } else if (self$wt == "pi") {
+            Xn = X
+            wn = rep(1, nrow(X))
+            zn = z
+        }
+        return(list(X=Xn,w=wn,z=as.factor(zn)))
+        }
+  )
+)
---
+++ tests/testthat/test_learner_lfbo.R
@@ -0,0 +1,54 @@
+test_that("LFBO Learner can be constructed and trained", {
+    l1 = lrn("classif.rpart", predict_type = "prob")
+    expect_class(l1, "LearnerClassif")
+    l2 = LearnerLFBO$new(l1)
+    expect_class(l2, "LearnerRegr")
+    s1 = SurrogateLearner$new(l2)
+    expect_class(s1, "SurrogateLearner")
+
+    t = tsk("boston_housing")
+    l2$train(t)
+    expect_true(!is.null(l2$state))
+    prd = l2$predict(t)
+    # Note: Truth value does not make sense!
+    expect_class(prd, "PredictionRegr")
+})
+
+test_that("LFBO Learner works for optim", {
+    set.seed(4321L)
+    funs = list(
+        fun = function(xs) {
+            list(y = xs$x ^ 2)
+        },
+        fun = function(xs) {
+        list(y = - xs$x ^ 2)
+        }
+    )
+    codomains = list(
+        ps(y = p_dbl(tags = "minimize")),
+        ps(y = p_dbl(tags = "maximize"))
+    )
+    for (i in seq_len(2)) {  
+        domain = ps(x = p_dbl(lower = -10, upper = 10))
+        fun = funs[[i]]
+        codomain = codomains[[i]]
+        objective = ObjectiveRFun$new(fun = fun, domain = domain, codomain = codomain)
+        instance = OptimInstanceSingleCrit$new(
+        objective = objective,
+        terminator = trm("evals", n_evals = 20)
+        )
+        surrogate = SurrogateLearner$new(LearnerLFBO$new(lrn("classif.ranger", predict_type = "prob", num.trees=2L)))
+        acq_function = acqf("mean")
+        acq_optimizer = acqo(
+        optimizer = opt("random_search", batch_size = 100),
+        terminator = trm("evals", n_evals = 100))
+        optimizer = opt("mbo",
+        loop_function = bayesopt_ego,
+        surrogate = surrogate,
+        acq_function = acq_function,
+        acq_optimizer = acq_optimizer
+        )
+        optimizer$optimize(instance)
+        expect_true(abs(instance$result$y) < .1)
+    }
+})
