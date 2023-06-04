0521efd301082038d54c18220445eafaf2d7b302
Lennart Schneider <lennart.sch@web.de>
2023-06-04 18:21:31 +0200

    use sobol sequence as default initial design for all loop functions (#121)

--- R/bayesopt_mpcl.R
+++ R/bayesopt_mpcl.R
@@ -18,7 +18,7 @@
 #'   Size of the initial design.
 #'   If `NULL` and the [bbotk::Archive] contains no evaluations, \code{4 * d} is used with \code{d} being the
 #'   dimensionality of the search space.
-#'   Points are drawn uniformly at random.
+#'   Points are generated via a Sobol sequence.
 #' @param surrogate ([Surrogate])\cr
 #'   [Surrogate] to be used as a surrogate.
 #'   Typically a [SurrogateLearner].
@@ -126,7 +126,7 @@ bayesopt_mpcl = function(
 
   # initial design
   if (isTRUE(init_design_size > 0L)) {
-    design = generate_design_random(domain, n = init_design_size)$data
+    design = generate_design_sobol(domain, n = init_design_size)$data
     instance$eval_batch(design)
   } else {
     init_design_size = instance$archive$n_evals
