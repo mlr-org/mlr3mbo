576a1f23a55e3cdb171162300f872f336a753299
Lennart Schneider <lennart.sch@web.de>
2023-06-05 09:09:24 +0200

    feat: add AcqFunctionSD

---
+++ R/AcqFunctionSD.R
@@ -0,0 +1,74 @@
+#' @title Acquisition Function Standard Deviation
+#'
+#' @include AcqFunction.R
+#' @name mlr_acqfunctions_sd
+#'
+#' @templateVar id sd
+#' @template section_dictionary_acqfunctions
+#'
+#' @description
+#' Posterior Standard Deviation.
+#'
+#' @family Acquisition Function
+#' @export
+#' @examples
+#' if (requireNamespace("mlr3learners") &
+#'     requireNamespace("DiceKriging") &
+#'     requireNamespace("rgenoud")) {
+#'   library(bbotk)
+#'   library(paradox)
+#'   library(mlr3learners)
+#'   library(data.table)
+#'
+#'   fun = function(xs) {
+#'     list(y = xs$x ^ 2)
+#'   }
+#'   domain = ps(x = p_dbl(lower = -10, upper = 10))
+#'   codomain = ps(y = p_dbl(tags = "minimize"))
+#'   objective = ObjectiveRFun$new(fun = fun, domain = domain, codomain = codomain)
+#'
+#'   instance = OptimInstanceSingleCrit$new(
+#'     objective = objective,
+#'     terminator = trm("evals", n_evals = 5))
+#'
+#'   instance$eval_batch(data.table(x = c(-6, -5, 3, 9)))
+#'
+#'   learner = lrn("regr.km",
+#'     covtype = "matern3_2",
+#'     optim.method = "gen",
+#'     nugget.stability = 10^-8,
+#'     control = list(trace = FALSE))
+#'
+#'   surrogate = srlrn(learner, archive = instance$archive)
+#'
+#'   acq_function = acqf("sd", surrogate = surrogate)
+#'
+#'   acq_function$surrogate$update()
+#'   acq_function$update()
+#'   acq_function$eval_dt(data.table(x = c(-1, 0, 1)))
+#' }
+AcqFunctionSD = R6Class("AcqFunctionSD",
+  inherit = AcqFunction,
+
+  public = list(
+
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    #'
+    #' @param surrogate (`NULL` | [SurrogateLearner]).
+    initialize = function(surrogate = NULL) {
+      assert_r6(surrogate, "SurrogateLearner", null.ok = TRUE)
+      super$initialize("acq_sd", surrogate = surrogate, requires_predict_type_se = TRUE, direction = "maximize", label = "Posterior Standard Deviation", man = "mlr3mbo::mlr_acqfunctions_sd")
+    }
+  ),
+
+  private = list(
+    .fun = function(xdt) {
+      p = self$surrogate$predict(xdt)
+      data.table(acq_sd = p$se)
+    }
+  )
+)
+
+mlr_acqfunctions$add("sd", AcqFunctionSD)
+
---
+++ tests/testthat/test_AcqFunctionSD.R
@@ -0,0 +1,24 @@
+test_that("AcqFunctionSD works", {
+  inst = MAKE_INST_1D()
+  surrogate = SurrogateLearner$new(REGR_FEATURELESS, archive = inst$archive)
+  acqf = AcqFunctionSD$new(surrogate = surrogate)
+  expect_acqfunction(acqf)
+
+  expect_r6(acqf$codomain, "ParamSet")
+  expect_equal(acqf$codomain$ids(), acqf$id)
+  expect_equal(acqf$surrogate_max_to_min, c(y = 1))
+  expect_equal(acqf$direction, "maximize")
+  expect_equal(acqf$domain, inst$search_space)
+  expect_learner(acqf$surrogate$learner)
+  expect_true(acqf$requires_predict_type_se)
+
+  design = MAKE_DESIGN(inst)
+  inst$eval_batch(design)
+
+  acqf$surrogate$update()
+  xdt = data.table(x = seq(-1, 1, length.out = 5L))
+  res = acqf$eval_dt(xdt)
+  expect_data_table(res, ncols = 1L, nrows = 5L, any.missing = FALSE)
+  expect_named(res, acqf$id)
+})
+
