0ed1969c648993557d53bc9db696696cceea787e
be-marc <marcbecker@posteo.de>
2024-02-12 11:29:52 +0100

    chore: import rush

--- R/zzz.R
+++ R/zzz.R
@@ -9,6 +9,7 @@
 #' @import lgr
 #' @import mlr3
 #' @import mlr3tuning
+#' @import rush
 #' @importFrom stats setNames runif dnorm pnorm quantile
 #' @useDynLib mlr3mbo c_sms_indicator c_eps_indicator
 "_PACKAGE"
