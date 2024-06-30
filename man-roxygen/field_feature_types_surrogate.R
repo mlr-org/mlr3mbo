02f512e0576f97ee5b375cacd62de28d44c57fda
be-marc <marcbecker@posteo.de>
2024-06-30 21:24:05 +0200

    fix: links

--- man-roxygen/field_feature_types_surrogate.R
+++ man-roxygen/field_feature_types_surrogate.R
@@ -1,3 +1,3 @@
 #' @field feature_types (`character()`)\cr
 #'   Stores the feature types the surrogate can handle, e.g. `"logical"`, `"numeric"`, or `"factor"`.
-#'   A complete list of candidate feature types, grouped by task type, is stored in [`mlr_reflections$task_feature_types`][mlr_reflections].
+#'   A complete list of candidate feature types, grouped by task type, is stored in [`mlr_reflections$task_feature_types`][mlr3::mlr_reflections].
--- man-roxygen/field_properties_surrogate.R
+++ man-roxygen/field_properties_surrogate.R
@@ -1,3 +1,3 @@
 #' @field properties (`character()`)\cr
 #'   Stores a set of properties/capabilities the surrogate has.
-#'   A complete list of candidate properties, grouped by task type, is stored in [`mlr_reflections$learner_properties`][mlr_reflections].
+#'   A complete list of candidate properties, grouped by task type, is stored in [`mlr_reflections$learner_properties`][mlr3::mlr_reflections].
