1674a5b1e38322d3a5fa6d660e5bdab702af4129
be-marc <marcbecker@posteo.de>
2025-07-08 17:52:49 +0200

    ...

--- R/SurrogateLearner.R
+++ R/SurrogateLearner.R
@@ -107,28 +107,34 @@ SurrogateLearner = R6Class("SurrogateLearner",
     #'
     #' @return [data.table::data.table()] with the columns `mean` and `se`.
     predict = function(xdt) {
-      assert_xdt(xdt)
-      xdt = fix_xdt_missing(copy(xdt), cols_x = self$cols_x, archive = self$archive)
+      # assert_xdt(xdt)
+
+      # xdt = fix_xdt_missing(copy(xdt), cols_x = self$cols_x, archive = self$archive)
+
       if (!is.null(self$input_trafo)) {
         xdt = self$input_trafo$transform(xdt)
       }
 
+
+      pred = self$learner$predict_newdata_fast(xdt)
+      pred = set_names(pred, c("mean", "se"))
+
       # speeding up some checks by constructing the predict task directly instead of relying on predict_newdata
-      task = self$learner$state$train_task$clone()
-      set(xdt, j = task$target_names, value = NA_real_)  # tasks only have features and the target but we have to set the target to NA
-      newdata = as_data_backend(xdt)
-      task$backend = newdata
-      task$row_roles$use = task$backend$rownames
-      pred = self$learner$predict(task)
+      # task = self$learner$state$train_task$clone()
+      # set(xdt, j = task$target_names, value = NA_real_)  # tasks only have features and the target but we have to set the target to NA
+      # newdata = as_data_backend(xdt)
+      # task$backend = newdata
+      # task$row_roles$use = task$backend$rownames
+      # pred = self$learner$predict(task)
 
       # slow
       #pred = self$learner$predict_newdata(newdata = xdt)
 
-      pred = if (self$learner$predict_type == "se") {
-        data.table(mean = pred$response, se = pred$se)
-      } else {
-        data.table(mean = pred$response)
-      }
+      # pred = if (self$learner$predict_type == "se") {
+      #   data.table(mean = pred$response, se = pred$se)
+      # } else {
+      #   data.table(mean = pred$response)
+      # }
       if (!is.null(self$output_trafo) && self$output_trafo$invert_posterior) {
         pred = self$output_trafo$inverse_transform_posterior(pred)
       }
