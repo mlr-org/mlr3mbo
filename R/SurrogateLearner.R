ab400d195a378f33a93e6fda04da6d94cd871672
be-marc <marcbecker@posteo.de>
2025-09-26 14:13:31 +0200

    ...

--- R/SurrogateLearner.R
+++ R/SurrogateLearner.R
@@ -115,27 +115,33 @@ SurrogateLearner = R6Class("SurrogateLearner",
         xdt = self$input_trafo$transform(xdt)
       }
 
-      pred = self$learner$predict_newdata_fast(xdt)
-      pred = set_names(pred, c("mean", "se"))
+      if (!inherits(self$learner, "GraphLearner")) {
+        pred = self$learner$predict_newdata_fast(xdt)
+        pred = set_names(pred, c("mean", "se"))
+      } else {
+        # speeding up some checks by constructing the predict task directly instead of relying on predict_newdata
+        task = self$learner$state$train_task$clone()
+        set(xdt, j = task$target_names, value = NA_real_)  # tasks only have features and the target but we have to set the target to NA
+        newdata = as_data_backend(xdt)
+        task$backend = newdata
+        task$row_roles$use = task$backend$rownames
+        pred = self$learner$predict(task)
 
-      # speeding up some checks by constructing the predict task directly instead of relying on predict_newdata
-      # task = self$learner$state$train_task$clone()
-      # set(xdt, j = task$target_names, value = NA_real_)  # tasks only have features and the target but we have to set the target to NA
-      # newdata = as_data_backend(xdt)
-      # task$backend = newdata
-      # task$row_roles$use = task$backend$rownames
-      # pred = self$learner$predict(task)
-
+        pred = if (self$learner$predict_type == "se") {
+          list(mean = pred$response, se = pred$se)
+        } else {
+          list(mean = pred$response)
+        }
+      }
       # slow
       #pred = self$learner$predict_newdata(newdata = xdt)
 
-      # pred = if (self$learner$predict_type == "se") {
-      #   data.table(mean = pred$response, se = pred$se)
-      # } else {
-      #   data.table(mean = pred$response)
-      # }
+
+
+
+
       if (!is.null(self$output_trafo) && self$output_trafo$invert_posterior) {
-        pred = self$output_trafo$inverse_transform_posterior(pred)
+        pred = self$output_trafo$inverse_transform_posterior(as.data.table(pred))
       }
       pred
     }
