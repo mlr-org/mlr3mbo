0cce880b870674cd6c998070155ca83012e78bdf
Marc Becker <33069354+be-marc@users.noreply.github.com>
2026-03-18 14:57:41 +0100

    refactor: remove rush backward compatibility (#207)

--- R/TunerADBO.R
+++ R/TunerADBO.R
@@ -61,7 +61,7 @@
 #'       terminator = trm("evals", n_evals = 10))
 #'
 #'     mirai::daemons(2)
-#'     rush::rush_plan(n_workers=2, worker_type = "remote")
+#'     rush::rush_plan(n_workers=2, worker_type = "mirai")
 #'
 #'     tnr("adbo", design_size = 4, n_workers = 2)$optimize(instance)
 #'     mirai::daemons(0)
